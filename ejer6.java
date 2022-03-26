d60f1f31a0a59c583d49d72769d0a49dfecb2ec3
Rocio104 <rociominero07@gmail.com>
2022-03-25 21:22:34 -0600

    ejerciciosguia1

---
+++ ejer6.java
@@ -0,0 +1,33 @@
+
+package ejercicios;
+
+
+public class ejer6 {
+    public static void main(String[] args) {
+        //Declaracion de variables
+        
+        double promedio, nota1, nota2, nota3, nota4, nota5;
+        
+        //Inicializar variables 
+        nota1 = 8.0 * 0.25;
+        nota2 = 7.5 * 0.15;
+        nota3 = 9.0 * 0.15;
+        nota4 = 8.0 * 0.20;
+        nota5 = 9.0 * 0.25;
+        
+        
+        promedio = nota1 + nota2 + nota3 + nota4 + nota5;
+        
+        
+        //uso de variables
+        
+        System.out.println("El promedio final es:" + promedio);
+        
+        
+        
+        
+        
+    }
+    
+    
+}
